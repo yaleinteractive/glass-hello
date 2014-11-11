640d1473a5a9a24ebe750a566343a49657f695b1
Dan Michaelson <dan@linkedbyair.net>
2014-11-11 12:34:07 +0000

    Get location every 30 seconds and send it, with a timestamp, as a pinned card. Includes fixes I made in the glass-prism npm module.

--- server.js
+++ server.js
@@ -1,15 +1,47 @@
+// uses glass-prism Node.js library to interface with Glass
 var prism = require("glass-prism");
 
-prism.init({
-    "client_id": "611399686787-pt8oftv6amo862rqoaqlc022aibb04hb.apps.googleusercontent.com",
-    "client_secret": "wfIvsdAF-XtiA9ASU19Uz7Ge",
-    "redirect_dir": "https://glass-hello-c9-danmichaelson.c9.io/oauth2callback",
-    "port": process.env.PORT
-}, function() {
-    console.log('Ready');
-    prism.all.insertCard({ text: "Hello, world!" });
+// load configuration for this instance
+var config = require("./config.json");
+config.port = process.env.PORT;
+
+prism.init(config, function(err) {
+    console.log('Ready!', err);
+    updateCard();
+    setInterval(updateCard, 30000);
 });
 
-prism.on('newclient', function(tokens) {
-    prism.insertCard({ text: "Hi new client" }, tokens);
-});
+prism.on('newclient', onNewClient);
+prism.on('subscription', onSubscription);
+
+function onNewClient(tokens) {
+    console.log('New client');
+	updateCard();
+};
+
+function onSubscription(err, payload) {
+    console.log('Subscription');
+	if (payload.item) {
+	    // Do something with payload.item (e.g. payload.item.text) and payload.data.token
+	}
+};
+
+function updateCard() {
+    console.log("Update card");
+    prism.all.mirrorCall(prism.mirror().locations.get({ "id": "latest" }), function(err, data) {
+        var lat, lon;
+        if (err) {
+            console.log('Location error', err);
+        }
+        else if (data) {
+            lat = data.latitude;
+            lon = data.longitude;
+        }
+    	prism.all.updateCard({ 
+    	    text: lat + ', ' + lon + ', ' + String(new Date()), 
+    	    isPinned: true, 
+    	    notification: { level: 'DEFAULT' }, 
+    	    sourceItemId: "glass-hello" 
+    	});
+    });
+}
